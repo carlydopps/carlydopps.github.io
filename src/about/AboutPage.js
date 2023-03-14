c3efee1f8beccde5de345920fd00d01d04576879
Carly Doppelheuer <carly.doppelheuer@gmail.com>
2023-03-14 14:10:29 -0500

    Add about page

--- src/about/AboutPage.js
+++ src/about/AboutPage.js
@@ -1,3 +1,40 @@
 export const AboutPage = () => {
-    return "About page"
+    return <main>
+        <section>
+            <img src="https://res.cloudinary.com/dupram4w7/image/upload/v1678406601/DSC_0088_be57xc.jpg" alt="Profile image"/>
+            <h1>Hi, I'm Carly!</h1>
+        </section>
+        <section>
+            <p>I'm an adventurous engineer who leans into learning, fails fast, and loves to build together with a team. I have a passion for authenticity, a strength for adaptability, and a respect for resiliency.</p>
+            <div>
+                <h2>Why</h2>
+                <p>I was drawn to programming through a love of math, language, and creativity. Since then, I have become even more intrigued by the computational yet creative mindset programming ignites. I'm continuously drawn to it because it’s different in the mindset you have to bring to the table. Software engineering is often a blank slate that you have to jump into to build and create. It can also be an intricate web where you have to find the loose thread that’s making the entire project unravel. You have to get down to the fundamentals and truly understand what each aspect of code is doing, or you can’t see how the entire thread connects.</p>
+                <p>I also love that in programming you learn not just about code, but you learn a lot about who you are as a teammate. You figure out who you are when you lead but more importantly who you are when you fail. </p>
+            </div>
+            <div>
+                <h2>When</h2>
+                <p>Around a year ago I left a job I loved because I found a passion for programming. I first discovered programming when I was working with graduate computer science students. I started studying the fundamentals to better understand the students' curriculum, and it sparked an ambition to use concepts of programming to increase efficiency and reduce error across all engineering programs. I dove into building new tools to automate tasks and streamline workflows. It was just a small curiosity for what programming was, but it turned into a career leap and I haven’t looked back since.</p>
+            </div>
+            <div>
+                <h2>Where</h2>
+                <p>I recently completed an immersive, full-time program at Nashville Software School where we focused on full stack fundamentals and remote team collaboration. We utilized JavaScript, React, Python, and Django and developed multiple web applications with CRUD functionality and RESTful APIs. We applied object-oriented programming daily through group and individual projects. The program then culminated in a web based project in a simulated company environment using Agile values.</p>
+            </div>
+            <h2>How</h2>
+            <p>I’m an autonomous learner who seeks out new projects and languages to expand my skills. I value the balance of humility to know when to ask for help along with autonomy to research errors and new concepts before doing so. I also value a deep understanding of the fundamentals and ensuring a strong foundation to build upon. I compare it to math in the way that the fundamentals are the building blocks you'll use again and again to conceptualize increasingly advanced topics.</p>
+        </section>
+        <section>
+            <div>
+                <img/>
+                <img/>
+                <img/>
+                <img/>
+            </div>                
+            <div>
+                <h5>Travel</h5>
+                <p> A few years ago, my husband and I decided to pursue a childhood dream of adventuring across the country. We spent two years traveling the country in a camper that we renovated together. Sometimes we were the only people we knew on a little road of an unknown town in a brand new state, and it taught me so much about enjoying the ups and downs of the adventure and the serendipity of chance encounters.</p>
+                <p>"When everything goes wrong, that's when the adventure begins." - Yvon Chouinard</p>
+                <p>Two years, twenty-nine states, four flat tires, and a lot of broken things later, it still feels like a dream.</p>
+            </div>
+        </section>
+    </main>
 }
