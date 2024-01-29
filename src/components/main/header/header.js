d3933f4c15683ac77be714ae98461e48efff64f9
Carly Doppelheuer <carly.doppelheuer@gmail.com>
2024-01-29 12:49:12 -0600

    update header img

--- src/components/main/header/header.js
+++ src/components/main/header/header.js
@@ -1,7 +1,7 @@
 export const Header = () => {
     return <div id='home' class='header'>
         <div class='header-img'>
-            <img src='https://res.cloudinary.com/dupram4w7/image/upload/v1706504939/IMG_1992_5_by7v5k.jpg'></img>
+            <img src='https://res.cloudinary.com/dupram4w7/image/upload/v1706553080/pexels-karolina-grabowska-8003529_j0k3ge_0fab9f.jpg'></img>
         </div>
         <div class='header-details'>
             <p class='header-details__subtitle'>Adventurous engineer who leans into learning, fails fast, and loves to build together with a team.</p>
